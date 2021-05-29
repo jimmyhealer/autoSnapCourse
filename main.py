9952c293dcf47aa927abac7f1e20549635b16f4f
jimmyhealer <yahing6066@gmail.com>
2021-05-30 02:44:19 +0800

    Update User and Add User.txt

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 def main():
+  f = open('./user.txt')
+  username = f.readline().strip()
+  password = f.readline().strip()
+  print(username, password)
   capa = DesiredCapabilities.CHROME
   capa["pageLoadStrategy"] = "none"
   ch_options = Options()
@@ -15,14 +19,12 @@ def main():
 
   driver.get('https://ais.ntou.edu.tw/Default.aspx')
   driver.implicitly_wait(5)
-  driver.find_element_by_name('M_PW').send_keys('*********')
-  driver.find_element_by_id('M_PORTAL_LOGIN_ACNT').send_keys('00957125')
+  driver.find_element_by_name('M_PW').send_keys(password)
+  driver.find_element_by_id('M_PORTAL_LOGIN_ACNT').send_keys(username)
   driver.find_element_by_id('LGOIN_BTN').click()
   driver.switch_to.frame('menuFrame')
   driver.find_element_by_id('Menu_TreeViewt1').click()
-  time.sleep(0.5)
   driver.find_element_by_id('Menu_TreeViewt30').click()
-  time.sleep(0.5)
   driver.find_element_by_id('Menu_TreeViewt40').click()
   driver.save_screenshot('./ac.png')
   driver.close()
